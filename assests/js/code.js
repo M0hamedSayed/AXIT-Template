a53e95dcabc4bfd817c545c1bbcec1b91ad359ab
Mohamed Sayed <76722244+M0hamedSayed@users.noreply.github.com>
2021-01-10 11:00:15 +0200

    second commit

--- assests/js/code.js
+++ assests/js/code.js
@@ -6,13 +6,13 @@ $(window).on('load', function () {
         $('.body_content').fadeIn('slow');  //To show web content
         AOS.init({ duration: 800, }); //start AOS animation
     });
-/***********************************************************************************************/
+    /***********************************************************************************************/
     /*smooth scroll when click on nav bar */
     $('header a[href*="#"]').on('click', function (e) {
         e.preventDefault(); //to prevent default events.
         $('body , html').animate({ scrollTop: $($(this).attr('href')).offset().top }, 1000, 'linear');
     });
-/************************************************************************************************/
+    /************************************************************************************************/
     let link = document.querySelectorAll('header a[href*="#"]');
     /*put color orange on desired link that i clicked on*/
     $('header a[href*="#"]').click(function (e) {
@@ -25,11 +25,15 @@ $(window).on('load', function () {
         /*then add color*/
         $(this).addClass('active');
     });
-/************************************************************************************************/
-    /*catch scroll*/
-    $(window).on("scroll", function () {
+    /************************************************************************************************/
+    /*refresh Aos animation only in wheel event*/
+    $(window).on('wheel', function () {
         /*first reset and refresh AOS animation*/
         AOS.refresh();
+    })
+    /*catch scroll*/
+    $(window).on("scroll", function () {
+
 
         /*if scroll down > 50
             >> animate header
@@ -49,7 +53,7 @@ $(window).on('load', function () {
             $(link).removeClass('active');
         }
     });
-/************************************************************************************************/
+    /************************************************************************************************/
     //tabs section event click on tab 1 , tab 2 , tab 3 buttons
     $('.tab_btn').click(function (e) {
         e.preventDefault();
@@ -81,13 +85,13 @@ $(window).on('load', function () {
             $('.tab3').slideToggle('slow');
         }
     })
-/************************************************************************************************/
+    /************************************************************************************************/
     // when pressed on back to top button
     $('#button').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: 0 }, 'slow');
     });
-/************************************************************************************************/
+    /************************************************************************************************/
     /*Dark mode
         >>First get attribute and check if web page in dark mode or light mode
         >>second change theme and change Title Image*/
@@ -104,7 +108,7 @@ $(window).on('load', function () {
         console.log(check)
 
     });
-/************************************************************************************************/
+    /************************************************************************************************/
     /*show burger button on mobile screen*/
     let button_burger = document.querySelector('.burger__input');
     let nav = document.querySelector('header .right_nav');
@@ -125,10 +129,9 @@ $(window).on('load', function () {
             nav.classList.remove('block');
         }
     }
-/************************************************************************************************/
+    /************************************************************************************************/
     /*show nav bar when click on burger button*/
     $('.burger__input').unbind("click").click(function (e) { //unbind() method removes event handlers from selected elements.
-        e.preventDefault();
         nav.classList.toggle('block');
     });
 });
